02fcab7a5a7276ad190ff1490d89240533f03b42
Keerthika S <skeerthika101@gmail.com>
2026-06-25 13:55:12 +0530

    Add files via upload

---
+++ lab day 1.py
@@ -0,0 +1,99 @@
+'''a=9
+b=8
+print(a+b)'''
+#Arithematic operators
+'''a=int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)'''
+
+#Area&perimeter
+'''l = int(input("Length: "))
+b = int(input("Breadth: "))
+
+print("Area =", l * b)
+print("Perimeter =", 2 * (l + b))'''
+
+#Square
+'''s = int(input("Side: "))
+
+print("Area =", s * s)
+print("Perimeter =", 4 * s)'''
+
+#Circle
+'''r = int(input("Radius: "))
+
+print("Area =", 3.14 * r * r)
+print("Circumference =", 2 * 3.14 * r)'''
+
+
+#Average of Three Numbers
+'''a = int(input("First number: "))
+b = int(input("Second number: "))
+c = int(input("Third number: "))
+
+avg = (a + b + c) / 3
+print("Average =", avg)'''
+
+#Compare Two Numbers
+'''a = int(input("First number: "))
+b = int(input("Second number: "))
+
+print(a == b)
+print(a > b)
+print(a <= b)'''
+
+#Square Root
+'''n = int(input("Enter a number: "))
+
+print("Square Root =", n ** 0.5)'''
+
+#Simple Interest and Compound Interest
+'''p = int(input("Principal: "))
+r = int(input("Rate: "))
+t = int(input("Time: "))
+
+si = p * r * t / 100
+
+print("Simple Interest =", si)'''
+#Assignment Operators
+'''x = 10
+x += 5
+x -= 3
+x *= 2
+x /= 4
+x %= 2
+x **= 3
+
+print(x)
+'''
+#Swapping Two Numbers
+'''a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("a =", a)
+print("b =", b)'''
+#Username and Password Check
+'''username = input("Username: ")
+password = input("Password: ")
+
+if username == "admin" and password == "123":
+    print("Login Successful")
+else:
+    print("Login Failed")'''
+#Cube Root of a Number (Simple)
+num = int(input("Enter a number: "))
+
+print("Cube Root =", num ** (1/3))
+
+
+
