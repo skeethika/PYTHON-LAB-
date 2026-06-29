6d45a505587c605c87f6f510f2339f4994e73d30
Keerthika S <skeerthika101@gmail.com>
2026-06-29 21:36:42 +0530

    Add files via upload

---
+++ lab 2 output.py
@@ -0,0 +1,233 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ===========
+enter marks:76
+pass
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+enter age:56
+eligible
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+enter age:17
+not eligible
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+enter bill:6000
+discount=10%
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+enter bill:4000
+no discount
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+Enter bill amount: 6000
+Final Amount = 5400.0
+
+= RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py
+Enter bill amount: 4000
+Final Amount = 4000.0
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+Enter password: 1234
+Access Granted
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter marks:89
+grade B
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter marks:24
+fail
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter first number:67
+enter second number:90
+enter +,-,*,/:-
+-23
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter first number:7
+enter second number:8
+enter +,-,*,/:+
+15
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter first number:89
+enter second number:67
+enter +,-,*,/:*
+5963
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter first number:78
+enter second number:67
+enter +,-,*,/:/
+1.164179104477612
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter units:67
+bill= 134
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter units:867
+bill= 4335
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter units:179
+bill= 537
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter age:5
+free
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter age:12
+fare =50
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter age:45
+fare=100
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter salary :60000
+enter credit score:800
+loan approved
+
+======== RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py =======
+enter salary :25000
+not eligible
+
+============ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ===========
+Enter academic percentage: 69
+Enter entrance score: 86
+Admission Granted
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter academic percentage: 70
+Enter entrance score: 56
+Admission Rejected
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter degree percentage: 60
+Not Eligible
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter degree percentage: 89
+Enter aptitude score: 70
+Not Selected
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter degree percentage: 69
+Enter aptitude score: 79
+Selected
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+enter marks percentage:78
+not eligible
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+enter marks percentage:89
+enter attendance:75
+attendance low
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter account balance: 6000
+Enter withdrawal amount: 5600
+Enter PIN: 1234
+Withdrawal Successful
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter account balance: 6800
+Enter withdrawal amount: 8900
+Insufficient Balance
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter username: admin
+Enter password: 1234
+Enter OTP: 1111
+Login Successful
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter username: admin
+Enter password: 1234
+Enter OTP: 2222
+Wrong OTP
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter username: admin
+Enter password: 1455
+Wrong Password
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter username: ben
+Wrong Username
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Doctor available? (yes/no): yes
+Slot available? (yes/no): yes
+Appointment Confirmed
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Doctor available? (yes/no): yes
+Slot available? (yes/no): no
+No Slots Available
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Registered? (yes/no): yes
+Fees Paid? (yes/no): yes
+Access Granted
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Registered? (yes/no): no
+Not Registered
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Registered? (yes/no): yes
+Fees Paid? (yes/no): no
+Pay Fees First
+
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter experience: 4
+Enter rating: 9
+Bonus Eligible
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter experience: 3
+Not Eligible
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Enter experience: 3
+Enter rating: 8
+Bonus Eligible
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Hall Ticket Available? (yes/no): yes
+Fees Cleared? (yes/no): yes
+Exam Access Granted
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Hall Ticket Available? (yes/no): yes
+Fees Cleared? (yes/no): no
+Clear Fees First
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Selected for admission? (yes/no): yes
+Room Available? (yes/no): yes
+Room Allotted
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Selected for admission? (yes/no): no
+Admission Not Confirmed
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Premium Member? (yes/no): yes
+Enter purchase amount: 7000
+Discount = 1400.0
+Final Amount = 5600.0
+>>> 
+================================================ RESTART: C:/Users/Keerthika/OneDrive/Documents/lab 2.py ================================================
+Premium Member? (yes/no): yes
+Enter purchase amount: 4500
+Discount = 0
+Final Amount = 4500.0
