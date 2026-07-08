ae2c7b0a4d0b0dfae881d04e0205b87f7743a1c5
Keerthika S <skeerthika101@gmail.com>
2026-07-08 14:18:00 +0530

    Add files via upload

---
+++ lab 5.py
@@ -0,0 +1,62 @@
+#task 1
+'''a="Keerthi"
+print(a.upper())
+print(a.lower())
+print(a.title())
+
+#task2
+email=input("enter email:")
+print(email[0].isalpha())
+print(email.endswith(".com"))
+
+#task3
+text= "livewire is an institute"
+print(text.count("i"))
+print(text.count("e"))
+
+#task4
+word="python"
+if "o" in word:
+    print(word.find("o"))
+else:
+    print("character not found")
+#task5
+password="admin1234"
+print(password.isalpha())
+print(password.isdigit())
+print(password.isalnum())
+#task6
+text="hello"
+print(text.center(10,"*"))
+print(text.ljust(10,"_"))
+print(text.rjust(10,"_"))
+#task7
+a="nothing is better"
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+
+
+#task 8
+text=input("enter a sentence:")
+print(text.replace("python","java"))
+#task9
+a=input("enter a roll number:")
+print(a.zfill(10))'''
+#task10
+a="keerthi"
+print(a.isupper())
+print(a.islower())
+print(a.istitle())
+
+
+
+
+
+
+
+
+
+
+                 
+    
